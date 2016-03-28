a2e060521b6a40591f61b9c8d4468f1db24adf45
Phrlog <shiftiam@yandex.ru>
2016-03-28 20:46:16 +0300

    оптимизация js

--- js/clicks.js
+++ js/clicks.js
@@ -2,10 +2,12 @@ $(function(){
     if ($.session.get('b_image') !== undefined) {
         var img = document.getElementById("b_image"); 
         img.src = $.session.get('b_image');
+        $('#back').attr('value', $.session.get('b_image'));        
     }
     if ($.session.get('f_image') !== undefined){
         var img = document.getElementById("f_image"); 
         img.src = $.session.get('f_image');
+        $('#letter').attr('value', $.session.get('f_image'));
     }
 });
 
@@ -15,14 +17,12 @@ $(document).on('click', '.emoj', function () {
         img.src = $(this).attr('id');
         $.session.set('b_image', $(this).attr('id'));
         $('#back').attr('value', $(this).attr('id'));
-        $('#back').trigger('click');
         console.log('#back');
     } else {
         var img = document.getElementById("f_image"); 
         img.src = $(this).attr('id');
         $.session.set('f_image', $(this).attr('id'));
         $('#letter').attr('value', $(this).attr('id'));
-        $('#letter').trigger('click');
         console.log('#letter');
     }
 });
