78ad3b7cd672d8994c0045bcbc04754da4af925d
Phrlog <shiftiam@yandex.ru>
2016-09-12 23:14:10 +0300

    improve coding style

--- templates/choose.php
+++ templates/choose.php
@@ -1,9 +1,6 @@
-<?php
-
-foreach ($data as $key => $value) {
-    $key = str_replace("_", "", $key);
-    echo "<div class='emoj' id='$value' data-title='$key'>";
-    echo "<img width = '25px' height = '25px' src={$value}>";
-    echo '</div>';
-}
-
+<?php foreach ($data as $key => $value): ?>
+    <?php $key = str_replace("_", "", $key); ?>
+    <div class='emoj' id='<?= $value ?>' data-title='<?= $key ?>'>
+    <img width = '25px' height = '25px' src=<?= $value ?>>
+    </div>
+<?php endforeach; ?>
--- templates/menu.php
+++ templates/menu.php
@@ -1,35 +1,7 @@
-<?php
-$category = [
-    "?category=all" => "Показать всё",
-    "?category=emotions" => "Эмоции",
-    "?category=love" => "Любовь",
-    "?category=gesture" => "Жесты",
-    "?category=weather" => "Погода",
-    "?category=food" => "Еда",
-    "?category=holiday" => "Праздники",
-    "?category=animals" => "Животные",
-    "?category=symbol" => "Символы",
-    "?category=sport" => "Спорт",
-    "?category=plants" => "Растения",
-    "?category=technology" => "Техника",
-    "?category=building" => "Здания",
-    "?category=games" => "Игры",
-    "?category=music" => "Музыка",
-    "?category=transport" => "Транспорт",
-    "?category=office" => "Офис",
-    "?category=space" => "Космос",
-    "?category=clothes" => "Одежда",
-    "?category=paints" => "Рисунки",
-    "?category=humans" => "Люди",
-    "?category=zodiac" => "Знаки зодиака",
-    "?category=arrows" => "Стрелки",
-    "?category=flags" => "Флаги",
-    "?category=numeric" => "Числа"];
-
-foreach ($category as $key => $value) {
-    if (strcasecmp(mb_substr($key, 10, mb_strlen($key, 'utf-8'), 'utf-8'), $active ) == 0) {
-        echo '<li role="presentation" class = "active"><a href="' . $key . '">' . $value . '</a></li>';
-    } else {
-        echo '<li role="presentation"><a href="' . $key . '">' . $value . '</a></li>';
-    }
-}
+<?php foreach (View::$category as $key => $value):?>
+    <?php if (strcasecmp(mb_substr($key, 10, mb_strlen($key, 'utf-8'), 'utf-8'), $active ) == 0): ?>
+        <li role="presentation" class = "active"><a href="<?= $key ?>"><?= $value ?></a></li>
+    <?php else: ?>
+        <li role="presentation"><a href="<?= $key ?>"><?= $value ?></a></li>
+    <?php endif;?>
+<?php endforeach; ?>
