a4b3672bfc490c49a012dfe36e917f8275f29211
Phrlog <shiftiam@yandex.ru>
2016-09-12 23:17:55 +0300

    refactor category

--- core/View.php
+++ core/View.php
@@ -2,7 +2,35 @@
 
 class View {
 
-    static function displayByCategory($data, $error, $active) {        
+    static public $category =  [
+        "?category=all" => "Показать всё",
+        "?category=emotions" => "Эмоции",
+        "?category=love" => "Любовь",
+        "?category=gesture" => "Жесты",
+        "?category=weather" => "Погода",
+        "?category=food" => "Еда",
+        "?category=holiday" => "Праздники",
+        "?category=animals" => "Животные",
+        "?category=symbol" => "Символы",
+        "?category=sport" => "Спорт",
+        "?category=plants" => "Растения",
+        "?category=technology" => "Техника",
+        "?category=building" => "Здания",
+        "?category=games" => "Игры",
+        "?category=music" => "Музыка",
+        "?category=transport" => "Транспорт",
+        "?category=office" => "Офис",
+        "?category=space" => "Космос",
+        "?category=clothes" => "Одежда",
+        "?category=paints" => "Рисунки",
+        "?category=humans" => "Люди",
+        "?category=zodiac" => "Знаки зодиака",
+        "?category=arrows" => "Стрелки",
+        "?category=flags" => "Флаги",
+        "?category=numeric" => "Числа"];
+
+
+    static function displayByCategory($data, $error, $active) {
         $content = "../templates/choose.php";
         include '../templates/default.php';
     }
